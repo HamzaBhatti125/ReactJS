07077565a178828c452df4c2171223a63509e38f
Hamza Bhatti <hbhati006@gmail.com>
2020-04-27 23:44:24 +0500

    last comit 27/4

--- react-redux-1/code/src/redux/actions/authorActions.js
+++ react-redux-1/code/src/redux/actions/authorActions.js
@@ -1,20 +1,22 @@
-import { LOAD_AUTHORS_SUCCESS } from './actionTypes';
+import * as types from "./actionTypes";
 import * as authorApi from "../../api/authorApi";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
 
-export function loadauthorsSuccess(authors) {
-    return { type: LOAD_AUTHORS_SUCCESS, authors };
-  }
-  
-  export function loadAuthors() {
-    return function(dispatch) {
-      return authorApi
+export function loadAuthorsSuccess(authors) {
+  return { type: types.LOAD_AUTHORS_SUCCESS, authors };
+}
+
+export function loadAuthors() {
+  return function(dispatch) {
+    dispatch(beginApiCall());
+    return authorApi
       .getAuthors()
-        .then(authors => {
-          dispatch(loadauthorsSuccess(authors));
-        })
-        .catch(error => {
-          throw error;
-        });
-    };
-  }
-  
+      .then(authors => {
+        dispatch(loadAuthorsSuccess(authors));
+      })
+      .catch(error => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
